35943a5bfebbcb4e51f9f0fc7628a1ca4142d87d
Ali Huzaifa <alihuzaifa2112006@gmail.com>
2025-06-27 00:47:56 +0500

    Fixed double Router error and applied best practices

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,18 @@ import Home from './Pages/Home'
 import Setting from './Pages/Setting'
 import Notes from './Pages/SNote'
 import Profile from './Pages/Profile'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-
-
   return (
     <>
-
-      <Router>
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/setting" element={<Setting />} />
-           <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Router>
-
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   )
 }
